06aae0011c1c09c189ba47d11fd1e752b913ae9f
mdmuhir <mdmuhir03@gmail.com>
2023-12-22 21:23:15 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+// import { setupCounter } from "./counter.js";
+
+// setupCounter(document.querySelector("#counter"));
+ import { setupCalculator } from "./calcutator";
+ setupCalculator(document.querySelector(".container"))
